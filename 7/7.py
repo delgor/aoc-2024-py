9a24d7a1a52ba0690365a453f915f1525b07be6d
Alexander SIol <1238171+delgor@users.noreply.github.com>
2024-12-10 16:02:45 +0100

    day 7 part 2

--- 7/7.py
+++ 7/7.py
@@ -16,7 +16,7 @@ for line in input_lines:
     equations.append(equation)
     #print(f"Equation to solve: {target} == {numbers} with operands unknown")
 
-def equation_solvable(target, numbers) -> bool:
+def equation_solvable(target, numbers, allow_concat=False) -> bool:
     if len(numbers) == 1:
         return target == numbers[0]
     elif len(numbers) == 0:
@@ -28,18 +28,29 @@ def equation_solvable(target, numbers) -> bool:
 
         # Does add solve it?
         combined_value = numbers[0] + numbers[1]
-        add_possible = equation_solvable(target, [combined_value, *remaining_numbers])
+        add_possible = equation_solvable(target, [combined_value, *remaining_numbers], allow_concat)
 
         # Does mul solve it?
         combined_value = numbers[0] * numbers[1]
-        mul_possible = equation_solvable(target, [combined_value, *remaining_numbers])
+        mul_possible = equation_solvable(target, [combined_value, *remaining_numbers], allow_concat)
 
-        return add_possible or mul_possible
+        # Does concat solve it?
+        concat_possible = False
+        if allow_concat:
+            combined_value = int(str(numbers[0]) + str(numbers[1]))
+            concat_possible = equation_solvable(target, [combined_value, *remaining_numbers], allow_concat)
+
+        return add_possible or mul_possible or concat_possible
 
 part1 = 0
+part2 = 0
 for target, numbers in equations:
     if equation_solvable(target, numbers):
         print(f"{target} == magic({numbers}) is solvable!")
         part1 += target
+    if equation_solvable(target, numbers, True):
+        print(f"{target} == magic({numbers}) is solvable with concat!")
+        part2 += target
 
 print(f"part1: {part1}")
+print(f"part2: {part2}")
